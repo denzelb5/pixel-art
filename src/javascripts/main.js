9fb0bc96c1962607cd902d8f97204eb9fe68710e
Denise Baker <denviol@yahoo.com>
2019-10-07 20:36:21 -0500

    added color picker buttons

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,11 @@
 import 'bootstrap';
 import '../styles/main.scss';
 import grid from './components/grid/grid';
+import paint from './components/paintBrush/paintbrush';
 
 const init = () => {
   grid.makeGrid();
+  paint.printColorPicker();
 };
 
 init();
